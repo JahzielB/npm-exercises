d1babf117410ab157151bbe347c6d91412ea3d4d
JahzielB <jahzielb@gmail.com>
2020-05-27 16:15:56 -0500

    finished modules exercise

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const $ = require('jquery');
+const {add, subtract, multiply, divide} = require("./add-function");
+import { niceStr } from "./say-hello";
 
 let sayHello = () => console.log("Hello");
 sayHello();
 
-$("body").css("background-color", "red");
+$("body").css("background-color", "green");
+console.log(niceStr());
+console.log(add(4, 5));
+console.log(multiply(3, 4));
---
+++ src/say-hello.js
@@ -0,0 +1,1 @@
+export const niceStr = () => "You're awesome!";
